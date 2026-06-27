897390df37398477f3b0150b553b4ed0aa82649b
DZDT <haiderben111@gmail.com>
2026-06-27 23:56:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("Welcome to DZDT");
+function copyContract() {
+    navigator.clipboard.writeText(
+        "CiAHefHCYdNrj9ne9JrSDpNr684YKaH58p5b9HvBpump"
+    );
+
+    alert("Contract address copied!");
+}
